284695b34bb3f3a3c68aabea19d62d3d159febb0
Rmurthy1 <rahul.a.murthy@gmail.com>
2024-09-20 02:21:33 -0700

    cleanup and preparing upload stuff

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POS
 const int _DoutPin = D7;
 const int _SckPin = D3;       
 long _offset = 0; // tare value
-int _scale = 10;
+int _scale = 100;
 
 // indicator led
 const int ledPin = D8;
@@ -44,14 +44,13 @@ SoftwareSerial ssrfid = SoftwareSerial(D5,D6); // RX, TX
 char buffer[BUFFER_SIZE]; // used to store an incoming data frame 
 int buffer_index = 0;
 
-
-
 unsigned extract_tag();
 long hexstr_to_value(char *str, unsigned int length);
 long getValue();
 void prepareScale();
 float readWeight();
 void useExtractedTag();
+void readRFIDSerialValue();
  
 
 
@@ -94,31 +93,35 @@ bool hasReadEnded = false;
 unsigned lastTag = 0;
 // rfid input debounce
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 500;    // the debounce time; increase if the output flickers
+unsigned long debounceDelay = 1000;    // the debounce time; increase if the output flickers
 
-bool isTagOk = false;
-bool firstReadOfSeries = false;
+bool isTagOk = false; // used to determine if the tag is valid (in useExtractedTag()), resets after each tag read. mutates in extract_tag()
+bool firstReadOfSeries = false; // used to determine if the first read of a series has been made, resets after 2 seconds of no serial reads
 unsigned long timeOfFirstRead = 0;
 bool doneReadingDueToTimeExpiring = false;
-bool initialRead = false;
+bool initialRead = false; // used to determine if the first read has been made, doesn't reset
 
 unsigned uploadTag = 0;
 int uploadWeight = 0;
-bool readyToUpload = false;
+//bool readyToUpload = false;
 
 // led blink stuff
 bool readyToRead = true;
 
+bool startUpload = false;
+bool uploadOnce = false; // do i really need this guy? or can i get away with startUpload?
+
 void loop() {
 
   if (network.isConnected() && initialRead == false) {
-    lcd.setCursor(0, 0);
-    lcd.print("Connected to wifi   ");
+    lcd.setCursor(9, 3);
+    lcd.print("wifi on");
   }
 
   if (hasReadStarted == true) {
     readyToRead = false;
-    if (millis() - timeSinceLastSerialReadFromRM6300 > debounceDelay) {
+    if (millis() - timeSinceLastSerialReadFromRM6300 > debounceDelay) { // the big reset happens here
+      Serial.println("inside of debounce delay for rfid, this should be called once data is done being sent from the rfid chip"); // called once
       hasReadStarted = false;
       hasReadEnded = true;
     }
@@ -126,16 +129,35 @@ void loop() {
       firstReadOfSeries = true;
       timeOfFirstRead = millis();
       initialRead = true;
+      startUpload = false;
+      lcd.clear();
     }
     if (doneReadingDueToTimeExpiring == true) {
-      
+      /*Serial.println("read ended due to the two second passing, data will continue to come from the chip"); // called many times
       lcd.setCursor(0, 0);
-      lcd.print("                    ");
+      lcd.print("                    ");*/ 
+      // this is where we should prepare for upload, we have the tag and the weight.
+      // just need to set a variable once and then we can upload
+      if (startUpload == false) {
+        startUpload = true;
+        uploadOnce = true;
+      }
+    }
+  }
+
+  if (startUpload == true) {
+    //Serial.println("upload test ");
+    if (uploadOnce == true) {
+      Serial.println("upload once");
+      uploadOnce = false;
+      lcd.setCursor(0, 2);
+      lcd.print("uploading ");
+      lcd.print(uploadTag);
     }
   }
 
   if (hasReadEnded == true) {
-    useExtractedTag();
+    //useExtractedTag(); // not sure why i had this here, it happens too late i think
     hasReadEnded = false;
     lastTag = 0; // unused?
     Serial.println("Read ended, ready for the next one");
@@ -144,20 +166,82 @@ void loop() {
     lcd.print("Ready to read tag");
     lcd.setCursor(0, 1);
     lcd.print("                  ");
+    lcd.setCursor(0, 2);
+    lcd.print("                  ");
     readyToRead = true;
   }
-
   digitalWrite(ledPin, ((readyToRead == true) && millis() % 1000 < 500));
+  readRFIDSerialValue();
+  // always update the scale value
+  
+  if (millis() > time_now + period) {
+    time_now = millis();
+    scaleValue = -1 * readWeight();
+    lcd.setCursor(0, 3);
+    lcd.print("        ");
+    lcd.setCursor(0, 3);
+    lcd.print(scaleValue);
+    lcd.print(" g");
+  }    
+}
+
+/*these are copilot comments, i just wanted to see what all the hype is about*/
+// this function is called when a tag has been read
+// it extracts the tag from the buffer and checks if the tag is valid
+// if the tag is valid, it will print the tag on the lcd
+// if the tag is not valid, it will not print the tag on the lcd
+// if the tag is valid, it will stop reading tags after 2 seconds
+// if the tag is not valid, it will stop reading tags immediately
+// if the tag is valid, it will store the tag and weight and prepare for upload
+// if the tag is not valid, it will not store the tag and weight and will not prepare for upload
+void useExtractedTag() { // called from readRFIDSerialValue()
+  unsigned tag = extract_tag();
+  if (isTagOk == false) {
+    return;
+  }
+
+  // after 2 seconds, we stop storing tags. 
+  if ((millis() - timeOfFirstRead > 2000) /*&& (tag != lastTag)*/) {
+    Serial.println("2 seconds have passed, stopping reading tags");
+    lcd.setCursor(0, 1);
+    lcd.print("Please remove box.");
+    doneReadingDueToTimeExpiring = true;
+    return;
+  } 
+  /*lcd.setCursor(0, 2);
+  lcd.print("                 ");  
+  lcd.setCursor(0, 2);
+  lcd.print(tag);*/
+  if (tag == 10622595) {
+    _offset = getValue();
+   
+  } else { // if its any other card
+    lcd.setCursor(0, 3);
+    lcd.print("                  ");
+    lcd.setCursor(8, 3);
+    lcd.print(scaleValue);
+    lcd.print(" g");
+  // store weight and tag and prepare for upload
+  uploadTag = tag;
+  uploadWeight = scaleValue;
+  //readyToUpload = true;
+
+  }
+}
 
 
+// called in loop, reads the serial value from the RFID reader
+// this mutates the buffer and buffer_index, timeSinceLastSerialReadFromRM6300, hasReadStarted
+void readRFIDSerialValue() {
+  
   if (ssrfid.available() > 0){
     hasReadStarted = true;
     timeSinceLastSerialReadFromRM6300 = millis();
 
     bool call_extract_tag = false;
-    Serial.println("Data available");
+    //Serial.println("Data available");
     int ssvalue = ssrfid.read(); // read
-    Serial.println(ssvalue); 
+    //Serial.println(ssvalue); 
     if (ssvalue == -1) { // no data was read
       Serial.println("Error: No data was read!");
       return;
@@ -185,56 +269,10 @@ void loop() {
       }
     }
   }
-
-  // always update the scale value
-  
-  if (millis() > time_now + period) {
-    time_now = millis();
-    scaleValue = -1 * readWeight();
-    lcd.setCursor(0, 3);
-    lcd.print("        ");
-    lcd.setCursor(0, 3);
-    lcd.print(scaleValue);
-    lcd.print(" g");
-  }    
 }
 
-void useExtractedTag() {
-  unsigned tag = extract_tag();
-  if (isTagOk == false) {
-    return;
-  }
-
-
-  // after 2 seconds, we stop printing tags. 
-  if ((millis() - timeOfFirstRead > 2000) /*&& (tag != lastTag)*/) {
-    Serial.println("2 seconds have passed, stopping reading tags");
-    lcd.setCursor(0, 1);
-    lcd.print("Please remove box.");
-    doneReadingDueToTimeExpiring = true;
-    // store weight and tag and prepare for upload
-    uploadTag = tag;
-    uploadWeight = scaleValue;
-    readyToUpload = true;
-    return;
-  } 
-
-  lcd.setCursor(0, 2);
-  lcd.print("                 ");  
-  lcd.setCursor(0, 2);
-  lcd.print(tag);
-  if (tag == 10622595) {
-    _offset = getValue();
-   
-  } else { // if its any other card
-    lcd.setCursor(0, 3);
-    lcd.print("                  ");
-    lcd.setCursor(8, 3);
-    lcd.print(scaleValue);
-    lcd.print(" g");
-  }
-}
-
+// extracts the tag from the buffer
+// this mutates the buffer, buffer_index, isTagOk, and returns the tag
 unsigned extract_tag() {
     char msg_head = buffer[0];
     char *msg_data = buffer + 1; // 10 byte => data contains 2byte version + 8byte tag
